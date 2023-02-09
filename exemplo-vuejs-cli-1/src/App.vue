a8ca0bf113bedd7a621850a935aba73f6d4c1711
Henrique-Ribeiro-glitch <64974578+Henrique-Ribeiro-glitch@users.noreply.github.com>
2023-02-08 21:36:35 -0300

    Add files via upload

---
+++ exemplo-vuejs-cli-1/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Ola message="System" />
+      <Image1 />
+    <HelloWorld msg="Welcome"/>
+    <Form />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Ola from './components/Ola.vue'
+import Image1 from './components/Image1.vue'
+import Form from './components/Form.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Ola,
+    Image1,
+    Form
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
